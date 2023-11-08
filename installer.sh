f48f81b022823b5ecb72858c8ee1aa6a81d8e6d9
yaron fanger <75136042+breadberry-prime@users.noreply.github.com>
2023-11-08 21:49:40 +0100

    Add anaconda channel and colorama dependency

--- installer.sh
+++ installer.sh
@@ -1,37 +1,88 @@
 #!/bin/bash
 
+# This script installs Anaconda, creates a Conda environment, and installs PyCharm Professional.
+# Usage:
+#   ./install_script.sh
+
+log_file="install_script.log"
+
+# Function to print a message with a timestamp
+log_message() {
+    if [ -t 1 ]; then
+        echo -e "[$(date +%Y-%m-%dT%H:%M:%S%z)] $1" | tee -a "$log_file"
+    else
+        echo "[$(date +%Y-%m-%dT%H:%M:%S%z)] $1" | tee -a "$log_file"
+    fi
+}
+
 # Function to display a message with a checkmark
 print_checkmark() {
-  echo -e "\\e[32m✔\\e[0m $1"
+    if [ -t 1 ]; then
+        log_message "\\e[32m✔\\e[0m $1"
+    else
+        log_message "✔ $1"
+    fi
 }
 
-# Function to display an error message
+# Function to display an error message and exit
 print_error() {
-  echo -e "\\e[31m✖ ERROR:\\e[0m $1" 1>&2
+    log_message "\\e[31m✖ ERROR:\\e[0m $1" >&2
+    exit 1
 }
+
+# Clean up function in case of failure
+cleanup() {
+    log_message "Cleaning up temporary files..."
+    # Add commands to clean up any temporary files or revert changes made by the script
+    log_message "Cleanup complete."
+}
+
+# Check for prerequisites before proceeding
+check_prerequisites() {
+    command -v wget >/dev/null 2>&1 || { echo >&2 "wget is required but it's not installed.  Aborting."; exit 1; }
+    command -v snap >/dev/null 2>&1 || { echo >&2 "snap is required but it's not installed.  Aborting."; exit 1; }
+}
+
+# Trap to catch any errors and execute the cleanup function
+trap cleanup ERR
+
+# Starting installation
+log_message "Starting installation process."
+
+check_prerequisites
 
 # Download the Anaconda installer
-wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh --quiet --show-progress > /dev/null 2>&1 || { print_error "Failed to download Anaconda installer."; exit 1; }
+log_message "Downloading Anaconda installer..."
+wget https://repo.anaconda.com/archive/Anaconda3-2023.09-0-Linux-x86_64.sh --quiet --show-progress >> "$log_file" 2>&1 || { print_error "Failed to download Anaconda installer."; }
 
-# Install Anaconda silently to the specified directory without manual intervention
-bash Anaconda3-2023.09-0-Linux-x86_64.sh -b -p "$HOME/anaconda3" > /dev/null 2>&1 && print_checkmark "Anaconda installed."
+# Install Anaconda silently
+log_message "Installing Anaconda..."
+bash Anaconda3-2023.09-0-Linux-x86_64.sh -b -p "$HOME/anaconda3" >> "$log_file" 2>&1 && print_checkmark "Anaconda installed."
 
-# Check if the conda.sh script exists and source it
+# Source conda.sh and initialize Conda
 if [ -f "$HOME/anaconda3/etc/profile.d/conda.sh" ]; then
-    source "$HOME/anaconda3/etc/profile.d/conda.sh" > /dev/null 2>&1
+    log_message "Sourcing Conda config..."
+    source "$HOME/anaconda3/etc/profile.d/conda.sh" >> "$log_file" 2>&1
     print_checkmark "Conda config sourced."
+    conda init bash >> "$log_file" 2>&1 && print_checkmark "Conda initialized."
 else
     print_error "Conda config file not found."
-    exit 1
 fi
 
-# Initialize Conda for future shell sessions
-conda init bash >> install.log 2>&1 && print_checkmark "Conda initialized."
-
 # Create the conda environment from the YAML file
-conda env create -f environment.yml > /dev/null 2>&1 && print_checkmark "Conda environment 'nlpenv' created." || print_error "Failed to create conda environment 'nlpenv'."
+log_message "Creating Conda environment 'nlpenv' from YAML file..."
+conda env create -f environment.yml >> "$log_file" 2>&1 && print_checkmark "Conda environment 'nlpenv' created." || print_error "Failed to create conda environment 'nlpenv'."
 
-# Install PyCharm Professional (This still requires sudo and is system-wide)
-sudo snap install pycharm-professional --classic > /dev/null 2>&1 && print_checkmark "PyCharm Professional installed." || print_error "Failed to install PyCharm Professional."
+# Install PyCharm Professional
+log_message "Installing PyCharm Professional..."
+sudo snap install pycharm-professional --classic >> "$log_file" 2>&1 && print_checkmark "PyCharm Professional installed." || print_error "Failed to install PyCharm Professional."
 
-echo -e "\e[32mInstallation successful!\e[0m Please run 'source ~/.bashrc' to enable conda and then 'conda activate nlpenv' to activate the environment."
+# Final message to the user
+print_checkmark "Installation successful!"
+log_message "Please run 'source ~/.bashrc' to enable conda and then 'conda activate nlpenv' to activate the environment."
+
+# Exit trap
+trap - ERR
+
+# Exit the script successfully
+exit 0
